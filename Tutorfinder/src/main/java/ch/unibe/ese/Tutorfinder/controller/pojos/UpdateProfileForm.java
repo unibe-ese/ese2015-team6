16b469a6388959a614eb28ea2d485c09e45315d7
Tuor Antonio <antonio@tuor.com>
2015-10-19 19:47:32 +0200

    Save to update Maven

--- Tutorfinder/src/main/java/ch/unibe/ese/Tutorfinder/controller/pojos/UpdateProfileForm.java
+++ Tutorfinder/src/main/java/ch/unibe/ese/Tutorfinder/controller/pojos/UpdateProfileForm.java
@@ -18,9 +18,9 @@ public class UpdateProfileForm {
 	private String region;
 	private String imgPath;
 	
-	/*TODO correct regex for valid exchange
-	@Pattern(regexp = "([0-9]{1,3})\.([0-9]?[05]?)", 
-		    message = "Must be valid email address")*/
+	
+	@Pattern(regexp = "([0-9]+)(\\.)([0-9]?)([05]?)", 
+		    message = "Must be valid wage in CHF")
 	private double wage;
 	
 	public long getId() {
