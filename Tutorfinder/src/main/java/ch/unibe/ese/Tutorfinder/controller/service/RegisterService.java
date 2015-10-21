771c7425817183e2697ab33f94be01e3a75d10a6
Florian Gerber <floriangerber@hotmail.com>
2015-10-21 12:57:12 +0200

    merged master into register branch

--- Tutorfinder/src/main/java/ch/unibe/ese/Tutorfinder/controller/service/RegisterService.java
+++
@@ -1,9 +0,0 @@
-package ch.unibe.ese.Tutorfinder.controller.service;
-
-import ch.unibe.ese.Tutorfinder.controller.exceptions.InvalidUserException;
-import ch.unibe.ese.Tutorfinder.controller.pojos.SignupForm;
-
-public interface RegisterService {
-
-	 public SignupForm saveFrom(SignupForm signupForm) throws InvalidUserException;
-}
