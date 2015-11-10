aaf4a2bbb0c7f0b37176e9524596c27ec4c180b1
Tuor Antonio <antonio@tuor.com>
2015-11-10 22:37:25 +0100

    add ControllerUnitTestSuite to main Test suite, add test for 100% coverage of services

--- tutorfinder/src/test/java/MainTestSuite.java
+++ tutorfinder/src/test/java/MainTestSuite.java
@@ -3,18 +3,18 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 import integrationTest.controller.ControllerIntegrationTestSuite;
+import unitTest.controller.ControllerUnitTestSuite;
 import unitTest.service.implementation.ServiceImplUnitTestSuite;
 
 @RunWith(Suite.class)
 @SuiteClasses({
 	ServiceImplUnitTestSuite.class,
-	ControllerIntegrationTestSuite.class
-	
+	ControllerIntegrationTestSuite.class,
+	ControllerUnitTestSuite.class
 })
 /**
  * Test suite including all tests
  * 
- * @author Antonio
  *
  */
 public class MainTestSuite {
---
+++ tutorfinder/src/test/java/unitTest/controller/ControllerUnitTestSuite.java
@@ -0,0 +1,17 @@
+package unitTest.controller;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({
+	ShowProfileControllerUnitTests.class
+})
+/**
+ * Test suite including all controller unit tests
+ *
+ */
+public class ControllerUnitTestSuite {
+
+}
