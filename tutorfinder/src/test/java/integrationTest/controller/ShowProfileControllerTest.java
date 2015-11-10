99d1f4de01dbd34a9a3df108038d46cd14914fa7
Nicola Pfister <nicola.pfister@students.unibe.ch>
2015-11-10 16:12:50 +0100

    Expanded PrepareFormService test

--- tutorfinder/src/test/java/integrationTest/controller/ShowProfileControllerTest.java
+++ tutorfinder/src/test/java/integrationTest/controller/ShowProfileControllerTest.java
@@ -1,7 +1,6 @@
 package integrationTest.controller;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.junit.Before;
